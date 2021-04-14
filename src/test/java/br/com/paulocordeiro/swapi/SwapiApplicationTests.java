20baf01cd0f1ca2893540eead6166d35bee3f25b
paulocordeiro <paulocord@gmail.com>
2021-04-14 16:01:32 -0300

    start project struct

---
+++ src/test/java/br/com/paulocordeiro/swapi/SwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulocordeiro.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
